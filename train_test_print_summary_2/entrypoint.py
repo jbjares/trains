a9e0bf878a7b59c9fb977979b2480521a6feb70a
lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>
2018-07-28 15:05:50 +0200

    Add: Train 2 for testing print_summary API

---
+++ train_test_print_summary_2/entrypoint.py
@@ -0,0 +1,16 @@
+from pht.train import Train, cmd_for_train
+
+class SummaryTrain(Train):
+
+    def print_next_departure_id(self):
+        raise NotImplemented
+
+    def run_algorithm(self):
+        raise NotImplemented
+
+    def print_summary(self):
+        print("TEST_PRINT_SUMMARY_2")
+
+if __name__ == '__main__':
+    cmd_for_train(SummaryTrain())
+
