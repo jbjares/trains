8e62f25dcd5dd55ad44c54e0c30f5b77a8c8717d
lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>
2018-08-01 10:06:27 +0200

    Add: Trains for checking requirements

---
+++ train_test_check_requirements_status_0/entrypoint.py
@@ -0,0 +1,20 @@
+from pht.train import Train, cmd_for_train
+import sys
+
+class SummaryTrain(Train):
+
+    def print_next_departure_id(self):
+        raise NotImplemented
+
+    def run_algorithm(self):
+        raise NotImplemented
+
+    def print_summary(self):
+        print("TEST_PRINT_SUMMARY_1")
+    
+    def check_requirements(self):
+        sys.exit(0)
+
+if __name__ == '__main__':
+    cmd_for_train(SummaryTrain())
+
