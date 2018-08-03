6e5a7fb72a89291393d7f04aa9fb03430aa8a856
lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>
2018-08-03 13:28:34 +0200

    Add: Train for counting rows of retrieved data

---
+++ train_test_count_rows/entrypoint.py
@@ -0,0 +1,40 @@
+from pht.train import Train, cmd_for_train
+from pht.station import StationClient
+import sys
+import tempfile
+
+def count_lines_in_file(filename):
+    with open(filename, 'r') as f:
+        return len(list(f))
+
+
+class CountRowsTrain(Train):
+
+    def __init__(self, hostname):
+        self.client = StationClient(hostname)
+        self.model = "/model"
+
+    def print_next_departure_id(self):
+        print(self.client.request_name())
+
+    def run_algorithm(self):
+        data_file = tempfile.mkstemp()[1]
+        self.client.request_data(data_file)
+        n_lines = count_lines_in_file(data_file)
+        name = self.client.request_name()
+        with open(self.model, 'w') as f:
+            f.write(name + '\t' + n_lines + '\n')
+
+    def print_summary(self):
+        with open(self.model, 'r') as f:
+            for line in f:
+                line = line.strip()
+                print(line)
+    
+    def check_requirements(self):
+        sys.exit(0)
+
+if __name__ == '__main__':
+    train = CountRowsTrain("station")
+    cmd_for_train(CountRowsTrain())
+
